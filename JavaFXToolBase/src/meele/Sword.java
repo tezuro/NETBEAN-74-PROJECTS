a3a7f818614c72ddab0167f8b6717cb0b2ba9f0a
tezuro <tezuro@NOOBSTER.localdomain>
2014-02-06 00:44:30 +0100

    Sword weitergemacht

--- JavaFXToolBase/src/meele/Sword.java
+++ JavaFXToolBase/src/meele/Sword.java
@@ -12,6 +12,8 @@ import java.util.List;
 import java.util.Random;
 import javafx.animation.SequentialTransition;
 import javafx.animation.Transition;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafxtoolbase.Damage;
 import meele.modifier.Modifier;
 
@@ -20,17 +22,21 @@ import meele.modifier.Modifier;
  * @author tezuro
  */
 public class Sword implements MeeleWeapon {
+
     private final TYPE type;
     private final List<Modifier> modi = new ArrayList<>();
     private final List<UnpurgeableBonus> weaponBonus = new ArrayList<>();
     final Player owner;
     final String weaponName;
-    protected double weaponWeight = 0.0;
+    protected final double weaponWeight;
+    protected final double weaponRange;
 
-    public Sword(final Player owner, final String weaponName, final TYPE type) {
+    public Sword(final Player owner, final String weaponName, final TYPE type, final double weight) {
+        this.weaponWeight = weight;
         this.type = type;
         this.owner = owner;
         this.weaponName = weaponName;
+        weaponRange = (weaponWeight / 100) + 70;
     }
 
     @Override
@@ -39,7 +45,8 @@ public class Sword implements MeeleWeapon {
         if (owner.STATS.armor.accuracyDebuff.doubleValue() < r.nextDouble()) {
             return new Damage(target, 0.0);
         }
-        Damage d = new Damage(target, 200.0);
+        final double damage = weaponWeight * owner.STATS.AGILITY.binding.get() / 100;
+        Damage d = new Damage(target, damage);
         target.STATS.armor.attack(d);
         for (Modifier m : modi) {
             m.hit(target, d);
@@ -50,14 +57,37 @@ public class Sword implements MeeleWeapon {
 
     @Override
     public boolean equipWeapon() {
-        if(TYPE.ONE_HAND.equals(type)&& weaponWeight > owner.STATS.MAX_WEAPON_WEIGHT_ONE_HANDED.doubleValue()){               
-            return false;
+
+        if (TYPE.ONE_HAND.equals(type)) {
+            if (weaponWeight > owner.STATS.MAX_WEAPON_WEIGHT_ONE_HANDED.doubleValue()) {
+                return false;
+            } else {
+                owner.STATS.MAX_WEAPON_WEIGHT_ONE_HANDED.addListener(new ChangeListener<Number>() {
+                    @Override
+                    public void changed(ObservableValue<? extends Number> ov, Number oldValue, Number newValue) {
+                        if (weaponWeight > newValue.doubleValue()) {
+                            unequipWeapon();
+                        }
+                    }
+                });
+            }
         }
-        if(TYPE.TWO_HAND.equals(type)&& weaponWeight > owner.STATS.MAX_WEAPON_WEIGHT_TWO_HANDED.doubleValue()){               
-            return false;
+        if (TYPE.TWO_HAND.equals(type)) {
+            if (weaponWeight > owner.STATS.MAX_WEAPON_WEIGHT_TWO_HANDED.doubleValue()) {
+                return false;
+            } else {
+                owner.STATS.MAX_WEAPON_WEIGHT_TWO_HANDED.addListener(new ChangeListener<Number>() {
+                    @Override
+                    public void changed(ObservableValue<? extends Number> ov, Number oldValue, Number newValue) {
+                        if (weaponWeight > newValue.doubleValue()) {
+                            unequipWeapon();
+                        }
+                    }
+                });
+            }
         }
-        
-        for (UnpurgeableBonus bonus:weaponBonus) {
+
+        for (UnpurgeableBonus bonus : weaponBonus) {
             bonus.equip(owner);
         }
         return true;
@@ -65,7 +95,7 @@ public class Sword implements MeeleWeapon {
 
     @Override
     public boolean unequipWeapon() {
-        for (UnpurgeableBonus bonus:weaponBonus) {
+        for (UnpurgeableBonus bonus : weaponBonus) {
             bonus.unequip(owner);
         }
         return true;
@@ -79,6 +109,16 @@ public class Sword implements MeeleWeapon {
     @Override
     public Transition getSkillAnimation(Player owner, Player target, Damage damage, SequentialTransition comboChainAnimation) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        /**
+         * hier muss die entsprechende animation hin 
+         * am ende der animation wird noch mal isSkillReady() aufgerufen und nur wenn dann true zurueckkommt wird damage gemacht
+         * weil wenn wir nicht mehr in range sind wird auch kein schaen gemacht
+         * 
+         * 
+         * schauen ob man fuer hoehere combocounts andere animationen macht
+         * 
+         * dauer der animation -> (W /  (STR*AGI)) * DEX /100
+         */
     }
 
     @Override
